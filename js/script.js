347b4bcbabc1584a73d88310cfea34ad80a75384
freyder_montano <fremontano24@gmail.com>
2024-02-18 10:29:17 -0300

    creando el menu de informacion

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+
+
+function runTypingEffect() {
+
+    const text = 'Soy, Freider Montaño.';
+    const typingElement = document.querySelector('#typing-text');
+    const typingDelay = 250;
+
+    typeText(text, typingElement, typingDelay);
+}
+
+
+function typeText(text, typingElement, delay) {
+    for (let i = 0; i < text.length; i++) {
+        setTimeout(() => {
+            typingElement.textContent += text.charAt(i);
+        }, delay * i);
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', runTypingEffect);
